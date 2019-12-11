11870a955f5a5bd28996b5fe7a91075f382013c2
ivan-drr <ivan.delrr@gmail.com>
2019-12-11 11:01:21 +0100

    Fix elementIsContainedIn

--- api/listFolderGoTo(building).php
+++ api/listFolderGoTo(building).php
@@ -1,7 +1,7 @@
 <?php
 header("access-control-allow-origin: http://localhost:3000");
 
-define('PATH', realpath('/home/snowtray/test/'));
+define('PATH', realpath('C:\Users\Nayxer\Projects\Atom\root'));
 
 $allFiles = array();
 $allKeys = array();
@@ -12,37 +12,12 @@ function manageElement(&$element) {
 
 function elementIsContainedIn($array, $element) {
     foreach ($array as $arrayElement) {
-        if (strstr($element, $arrayElement) != false) {
+        if (strpos($arrayElement, basename($element)) != false) {
             return true;
         }
     }
     return false;
 }
-
-// PROBLEMA: $char NO CONTIENE $str (DICE)
-
-$arr = ["/home/snowtray/test/3.txt", "/home/snowtray/test/folder1", "folder2"];
-$files = ["/home/snowtray/test/3.txt", "/home/snowtray/test/folder1/folder1.2/folder1.2.3"];
-
-$str = "/home/snowtray/test/folder1";
-$char = "/home/snowtray/test/folder1/folder1.2/folder1.2.3";
-
-$str = str_replace("/home/snowtray/", "", $str);
-$str = str_replace("/", " ", $str);
-
-//$str = substr($str, strpos(" ", $str));
-echo $str;
-//echo $str;
-
-echo strpos('/', $str);
-
-//echo strpos($str, $char)?'true':'false';
-
-//foreach ($arr as $elm) {
-    //echo elementIsContainedIn($files, $elm)?'si':'no';
-//}
-
-
 
 function recursivo(&$array, $path) {
     recursive:
@@ -64,7 +39,7 @@ function recursivo(&$array, $path) {
     }
 }
 
-//recursivo($allFiles, PATH);
+recursivo($allFiles, PATH);
 
 foreach ($allFiles as &$file) {
     $file .= is_dir($file)?'/':'';
