16b915806c9912c45d11f0b3fa67925ea954ac92
ivan-drr <ivan.delrr@gmail.com>
2020-05-25 20:09:43 +0200

    enhance performance unused css load on index.html change class components to functionals

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-class Breadcrumb extends Component {
-  render() {
-    let currentPath;
-    if (this.props.currentPath === '/') currentPath = 'root';
-    else currentPath = `root/${this.props.currentPath}`;
+export default function Breadcrumb(props) {
+  let currentPath;
+  if (props.currentPath === '/') currentPath = 'root';
+  else currentPath = `root/${props.currentPath}`;
 
-    return (
-      <InputGroup className="mb-3">
-        <InputGroup.Text id="pathText">
-          <span style={{ color: '#377e8c' }}>❖ Current path being spotted</span>
+  return (
+    <InputGroup className="mb-3">
+      <InputGroup.Text id="pathText">
+        <span style={{ color: '#377e8c' }}>❖ Current path being spotted</span>
+      </InputGroup.Text>
+      <InputGroup.Prepend>
+        <InputGroup.Text id="pathInfo" style={{ fontWeight: 'bold' }}>
+          <span style={{ color: 'white' }}>
+            〈
+            {currentPath}
+            〉
+          </span>
         </InputGroup.Text>
-        <InputGroup.Prepend>
-          <InputGroup.Text id="pathInfo" style={{ fontWeight: 'bold' }}>
-            <span style={{ color: 'white' }}>
-              〈
-              {currentPath}
-              〉
-            </span>
-          </InputGroup.Text>
-        </InputGroup.Prepend>
-      </InputGroup>
-    );
-  }
+      </InputGroup.Prepend>
+    </InputGroup>
+  );
 }
-export default Breadcrumb;
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,53 +1,44 @@
 import React, { Component } from 'react';
 import '../styles/Loading.css';
 
-class Loading extends Component {
-  constructor(props) {
-    super(props);
-    this.typeWriter = this.typeWriter.bind(this);
-  }
+export default function Loading(props) {
+  const iterator = 0;
 
-  typeWriter(str, speed, id, iterator, intervals) {
-    if (document.getElementById(id) !== null) {
-      const element = document.getElementById(id);
-
-      setTimeout(() => {
-        if (iterator < str.length) {
-          element.innerHTML += str.charAt(iterator);
-          iterator++;
-
-          setTimeout(this.typeWriter(str, speed, id, iterator), speed);
-
-          return true;
-        } return false;
-      }, 1000 / speed * 2);
-    }
-  }
-
-  render() {
-    const iterator = 0;
-
-    if (this.props._isFetch === 'error') {
-      setTimeout(() => {
-        this.typeWriter(' ✘ Error connecting to API', 70, 'errorText', iterator);
-      }, 0);
-      return (
-        <div id="loading" className="text-danger fixed-top">
-          <span id="errorText" className="ml-3" />
-        </div>
-      );
-    } if (!this.props._isFetch) {
-      setTimeout(() => {
-        this.typeWriter(' Loading...', 70, 'loadingText', iterator);
-      }, 0);
-      return (
-        <div id="loading" className="text-primary fixed-top">
-          <div className="spinner-border text-primary" role="status" />
-          <span id="loadingText" className="ml-3" />
-        </div>
-      );
-    } return null;
-  }
+  if (props._isFetch === 'error') {
+    setTimeout(() => {
+      typeWriter(' ✘ Error connecting to API', 70, 'errorText', iterator);
+    }, 0);
+    return (
+      <div id="loading" className="text-danger fixed-top">
+        <span id="errorText" className="ml-3" />
+      </div>
+    );
+  } if (!props._isFetch) {
+    setTimeout(() => {
+      typeWriter(' Loading...', 70, 'loadingText', iterator);
+    }, 0);
+    return (
+      <div id="loading" className="text-primary fixed-top">
+        <div className="spinner-border text-primary" role="status" />
+        <span id="loadingText" className="ml-3" />
+      </div>
+    );
+  } return null;
 }
 
-export default Loading;
+const typeWriter = (str, speed, id, iterator, intervals) => {
+  if (document.getElementById(id) !== null) {
+    const element = document.getElementById(id);
+
+    setTimeout(() => {
+      if (iterator < str.length) {
+        element.innerHTML += str.charAt(iterator);
+        iterator++;
+
+        setTimeout(typeWriter(str, speed, id, iterator), speed);
+
+        return true;
+      } return false;
+    }, 1000 / speed * 2);
+  }
+}
