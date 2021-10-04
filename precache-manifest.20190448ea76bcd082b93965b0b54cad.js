485deb87d089193522bc163ab52e8b5786e53ff8
ivan-drr <ivan.delrr@gmail.com>
2021-10-04 20:25:36 +0200

    Updates

---
+++ precache-manifest.20190448ea76bcd082b93965b0b54cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2a795a053dc29fcc2bf6a7d97f6814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15783c3e583861eb724e",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "6fa96391c292f93e80bb",
+    "url": "/static/css/main.517dbb7b.chunk.css"
+  },
+  {
+    "revision": "15783c3e583861eb724e",
+    "url": "/static/js/2.04e9b714.chunk.js"
+  },
+  {
+    "revision": "edcbe335938914f190add331be79d3c4",
+    "url": "/static/js/2.04e9b714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa96391c292f93e80bb",
+    "url": "/static/js/main.fdd7b376.chunk.js"
+  },
+  {
+    "revision": "1febb7c6a28dd78c1f54",
+    "url": "/static/js/runtime-main.9f44bfd1.js"
+  }
+]);
